67cea8ca25a30bfd7671e34e240c3bdca1507aaf
Corentinvls <corentin.vallois@gmail.com>
2020-12-08 10:40:50 +0100

    ça pu du cul

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import {ThemeProvider} from '@material-ui/core/styles';
+import mainTheme from "./style/MainTheme";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <ThemeProvider theme={mainTheme}>
+                <div className="App">
+                    <Navbar/>
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </ThemeProvider>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/style/MainTheme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import yellow from "@material-ui/core/colors/yellow";
+
+const mainTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: yellow[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+});
+export default mainTheme;

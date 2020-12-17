c6b912959d462610faf9f86469b471c407cd20d6
Corentinvls <corentin.vallois@gmail.com>
2020-12-17 14:23:54 +0100

    editProfile done

--- src/View/ProfileView.js
+++ src/View/ProfileView.js
@@ -7,6 +7,10 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import EditableAvatar from "../components/profile/EditableAvatar";
 import {logOut, updateUser} from "../store/actions";
 import {setUpdateUser} from "../request/userRequest";
+import EditIcon from "@material-ui/icons/Edit";
+import Badge from "@material-ui/core/Badge";
+import Button from "@material-ui/core/Button";
+import EditableField from "../components/profile/EditableField";
 
 const useStyles = makeStyles((theme) => ({
     large: {
@@ -17,18 +21,20 @@ const useStyles = makeStyles((theme) => ({
 
 function ProfileView(props) {
     const classes = useStyles();
-    const [user,setUser]=React.useState({"id": "",
-        "pseudo" : "",
+    const [user, setUser] = React.useState({
+        "id": "",
+        "pseudo": "",
         "mail": "",
-        "avatar" : ""})
-    const [,setState]=React.useState()
+        "avatar": ""
+    })
+    const [, setState] = React.useState()
+
     const handleChange = (field, value) => {
         const data = {
             "user": {
                 [field]: value
             }
         }
-
         setUpdateUser(data).then((response) => {
             console.log(response)
             props.updateUser(response.success, response.token)
@@ -46,18 +52,19 @@ function ProfileView(props) {
             <Grid
                 container
                 direction="column"
-                justify="space-evenly"
+                justify="space-between"
                 alignItems="center"
+                spacing={3}
             >
                 <Grid item>
-                <EditableAvatar pseudo={user.pseudo} avatar={user.avatar}
-                                handleChangeAvatar={(value) => handleChange("avatar",value)}/>
+                    <EditableAvatar pseudo={user.pseudo} avatar={user.avatar}
+                                    handleChangeAvatar={(value) => handleChange("avatar", value)}/>
                 </Grid>
-                <Grid  container
-                       direction="row"
-                       justify="space-evenly"
-                       alignItems="center">
-                  <h2>{user.pseudo}</h2>   <h2>{user.mail}</h2>
+                <Grid container
+                      direction="row"
+                      justify="space-evenly">
+                    <EditableField field={user.pseudo} handleChangeField={(value) => handleChange("pseudo", value)}/>
+                    <EditableField field={user.mail} handleChangeField={(value) => handleChange("mail", value)}/>
                 </Grid>
             </Grid>
         </Paper>
---
+++ src/components/profile/EditableField.js
@@ -0,0 +1,74 @@
+import React, {useRef} from 'react';
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import EditIcon from "@material-ui/icons/Edit";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
+import DialogChangeField from "./DialogChangeField";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+const useStyles = makeStyles((theme) => ({}));
+
+export default function EditableField(props) {
+    const classes = useStyles();
+    const [edit, setEdit] = React.useState(false)
+    const [field, setField] = React.useState(props.field)
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const isInitialMount = useRef(true);
+    React.useEffect(() => {
+        if (isInitialMount.current) {
+            setField(props.field)
+        }
+    }, [props])
+    return (
+        <div>{!edit ?
+            <Button
+
+                color="primary"
+                size="large"
+                className={classes.button}
+                endIcon={<EditIcon/>}
+                onClick={() => setEdit(true)}
+            >
+                {field}
+            </Button>
+            : <>
+                <TextField value={field} onChange={(event) => {
+                    setField(event.target.value)
+                }}/>
+                <ButtonGroup>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.button}
+                        onClick={handleClickOpen}
+                    ><SaveIcon  size="small"/>
+                    </Button>
+                    <Button
+                        variant="contained"
+                        style={{backgroundColor:"#e34b4b"}}
+                        size="small"
+                        className={classes.button}
+                        onClick={() => setEdit(false)}
+                    ><CancelIcon  />
+                    </Button>
+                </ButtonGroup>
+
+                <DialogChangeField open={open} handleClose={handleClose} handleChangeField={()=> props.handleChangeField(field)}/>
+
+            </>
+        }
+
+        </div>
+    );
+}

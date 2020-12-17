3e55fe152a709ab1cef48eadb1b2cef728d29b86
Corentinvls <corentin.vallois@gmail.com>
2020-12-17 12:05:05 +0100

    wip

--- src/View/ProfileView.js
+++ src/View/ProfileView.js
@@ -1,12 +1,85 @@
 import React from 'react';
+import {connect} from "react-redux/";
 
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import EditableAvatar from "../components/profile/EditableAvatar";
+import {logOut, updateUser} from "../store/actions";
+import {setUpdateUser} from "../request/userRequest";
+
+const useStyles = makeStyles((theme) => ({
+    large: {
+        width: theme.spacing(14),
+        height: theme.spacing(14),
+    },
+}));
 
 function ProfileView(props) {
+    const classes = useStyles();
+    const [user,setUser]=React.useState({"id": "",
+        "pseudo" : "",
+        "mail": "",
+        "avatar" : ""})
+    const [,setState]=React.useState()
+    const handleChange = (field, value) => {
+        const data = {
+            "user": {
+                [field]: value
+            }
+        }
 
+        setUpdateUser(data).then((response) => {
+            console.log(response)
+            props.updateUser(response.success, response.token)
+        })
+    }
+    React.useEffect(() => {
+        if (props.user) {
+            setUser(props.user)
+            setState("ready")
+        }
+    }, [props])
     return (
-        <div>YOLO</div>
+
+        <Paper style={{backgroundColor: '#f4f5f7'}}>
+            <Grid
+                container
+                direction="column"
+                justify="space-evenly"
+                alignItems="center"
+            >
+                <Grid item>
+                <EditableAvatar pseudo={user.pseudo} avatar={user.avatar}
+                                handleChangeAvatar={(value) => handleChange("avatar",value)}/>
+                </Grid>
+                <Grid  container
+                       direction="row"
+                       justify="space-evenly"
+                       alignItems="center">
+                  <h2>{user.pseudo}</h2>   <h2>{user.mail}</h2>
+                </Grid>
+            </Grid>
+        </Paper>
+
     );
 }
 
-export default ProfileView
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        logOut: () => dispatch(logOut()),
+        updateUser: (user, token) => dispatch(updateUser(user, token)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileView)
+
+
 
--- src/request/userRequest.js
+++ src/request/userRequest.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 async function postUser(data, url) {
     return await axios.post(url, data).then(result => {
+        return result.data
+    }).catch(
+        err => {
+            return err.response.data;
+        }
+    );
+}
+
+async function updateUser(data, url, config = {}) {
+    return await axios.post(url, data, config).then(result => {
         return result.data
     }).catch(
         err => {
@@ -18,5 +29,12 @@ export async function setSignUp(data) {
 export async function setSignIn(data) {
     let urlRoutePostSignUp = "http://185.163.126.173:4021/api/user-signin";
     return await postUser(data, urlRoutePostSignUp);
+}
+
+export async function setUpdateUser(data) {
+    const config = {headers: {'Authorization': 'Bearer ' + new Cookies().get('token')}}
+    let urlRoutePostSignUp = "http://185.163.126.173:4021/api/update-users";
+    console.log(data, urlRoutePostSignUp,config)
+    return await updateUser(data, urlRoutePostSignUp,config);
 }
 
